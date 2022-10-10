f487457a422cc30f0d85a9d22d55ec8e0336f567
KRISHNA-MANDALA <kireetigoud12@gmail.com>
2022-10-10 12:11:16 +0100

    java script

---
+++ script1.js
@@ -0,0 +1,8 @@
+document.write("Hello, world!");
+var name = "Krishna111";  // string type
+        var age = 23;  //Number type
+        var teacher = false; // Boolean type: Notice all lowercase true
+        document.write('Name ' + name + '<br>');  // + is used for string concatentation
+        document.write('Age ' + age + '<br>');  // Try using + with another number and see what happens!
+        document.write('Is teacher? ' + teacher);
+        console.log(name);
